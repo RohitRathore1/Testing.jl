46a24ffdcffffbec373ef9d131b6eb0ed7bfe114
RohitRathore1 <rohitrathore.imh55@gmail.com>
2021-09-23 10:16:41 +0530

    fixed bugs

--- Example1.jl
+++ Example1.jl
@@ -2,9 +2,11 @@ using VTKDataIO
 using PyCall
 using VTKDataTypes
 using DashVtk
+using Dash
+using DashHtmlComponents
 
-dash = pyimport("dash")
-dash_html = pyimport("dash_html_components")
+#dash = pyimport("dash")
+#dash_html = pyimport("dash_html_components")
 dash_vtk = pyimport("dash_vtk")
 x = y = z = [-2, -1, 0, 1, 2];
 rect = VTKRectilinearData((x, y, z));
@@ -17,29 +19,21 @@ dash_vtk_utils.to_mesh_state
 state = dash_vtk_utils.to_mesh_state(pyunstruct)
 
 content = vtk_view([
-    vtk_geometryrepresentation([
+        vtk_geometryrepresentation([
         vtk_mesh(state=state)
     ]),
 ])
 
-app = dash.Dash("__name__")
-server = app.server
+app = dash()
 
-#app.layout = html_div() do 
-#    html_div(
-#    style = Dict(
-#        "width" => "100%", 
-#        "height" => "400px"
-#    ),
-#    children=[content],
-#    )
-#end
-
-app.layout = dash_html.Div(
-    style=Dict(
+app.layout = html_div() do 
+    html_div(
+    style = Dict(
         "width" => "100%", 
         "height" => "400px"
-        ),
+    ),
     children=[content],
-)
+    )
+end
 
+run_server(app, "0.0.0.0", debug = true)
